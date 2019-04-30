8b2235aac8bc262f05cdd01aad9f1ffd60910085
Gagandeep Singh Ahuja <gagandeepahuja09@gmail.com>
2019-04-30 12:36:37 +0530

    good

--- InterviewBit/MatrixSearch.cpp
+++ InterviewBit/MatrixSearch.cpp
@@ -1,58 +1,38 @@
-int binarySearch(vector<vector<int>>& A, int i, int jLow, int jHigh, 
-int x) {
-    while(jLow <= jHigh) {
-        int jMid = (jLow + jHigh) / 2;
-        if(A[i][jMid] == x) {
+int bSearch(vector<vector<int>>& A, int r, int l, int h, int target) {
+    while(l <= h) {
+        int mid = l + (h - l) / 2;
+        if(A[r][mid] == target)
             return 1;
-            // cout << i << " " << j << endl;
-        }
-        else if(x < A[i][jMid]) {
-            jHigh = jMid - 1;
-        }
-        else {
-            jLow = jMid + 1;
-        }
+        else if(A[r][mid] < target)
+            l = mid + 1;
+        else
+            h = mid - 1;
     }
     return 0;
 }
 
-int Solution::searchMatrix(vector<vector<int> > &A, int B) {
-    int n = A.size(), m = A[0].size();
-    if(n == 1) {
-        return binarySearch(A, 0, 0, m - 1, B);
-    }
-    // Do binary search on middle column 
-    // Condition to terminate => 2 Rows remaining
-    int iLow = 0, iHigh = n-1, jMid = m/2;
-    while((iLow + 1) < iHigh) {
-        int iMid = (iLow + iHigh) / 2;
-        if(A[iMid][jMid] == B) {
+int Solution::searchMatrix(vector<vector<int> > &A, int target) {
+    int m = A.size(), n = A[0].size() - 1;
+    int l = 0, h = A.size() - 1;
+    while(h - l > 1) {
+        int mid = l + (h - l) / 2;
+        if(A[mid][n / 2] == target)
             return 1;
-        }
-        else if(B < A[iMid][jMid]) {
-            iHigh = iMid;
-        }
-        else {
-            iLow = iMid;
-        }
+        else if(A[mid][n / 2] < target)
+            l = mid;
+        else
+            h = mid;
     }
-    if(A[iLow][jMid] == B)
+    // cout << l << " " << h << endl;
+    if(A[l][n / 2] == target || A[h][n / 2] == target)
         return 1;
-    if(A[iLow + 1][jMid] == B)
-        return 1;    
-    // 1st half of 1st row
-    if(B <= A[iLow][jMid - 1])    
-        return binarySearch(A, iLow, 0, jMid - 1, B);
-    // 2nd half of 1st row    
-    else if(B >= A[iLow][jMid + 1] && B <= A[iLow][m - 1])
-        return binarySearch(A, iLow, jMid + 1, m - 1, B);
-        
-    // 2nd half of 1st row
-    else if(B <= A[iLow + 1][jMid - 1])    
-        return binarySearch(A, iLow + 1, 0, jMid - 1, B);
-    // 2nd half of 2nd row    
-    else if(B >= A[iLow + 1][jMid + 1] && B <= A[iLow + 1][m - 1])
-        return binarySearch(A, iLow + 1, jMid + 1, m - 1, B);    
-    return 0;    
+    if(target >= A[l][0] && target <= A[l][n / 2])
+        return bSearch(A, l, 0, n / 2, target);
+    else if(target >= A[l][n / 2] && target <= A[l][n])
+        return bSearch(A, l, n / 2, n, target);
+    else if(target >= A[h][0] && target <= A[h][n / 2])
+        return bSearch(A, h, 0, n / 2, target);
+    else
+        return bSearch(A, h, n / 2, n, target);
 }
 
