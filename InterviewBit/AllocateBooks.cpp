d472bfeff34417e835bfcc27331076dc609f489b
Gagandeep Singh Ahuja <gagandeepahuja09@gmail.com>
2019-05-01 13:09:55 +0530

    easy

--- InterviewBit/AllocateBooks.cpp
+++ InterviewBit/AllocateBooks.cpp
@@ -1,41 +1,36 @@
+#define ll long long int
 
-
-    bool isPossible(vector<int>& A, int M, int currMin) {
-        int currSum = 0, studentsReq = 1;
-        for(int i=0; i < A.size(); i++) {
-            if(A[i] > currMin)
-                return false;
-            if(currSum + A[i] > currMin) {
-                studentsReq++;
-                currSum = A[i];
-                if(studentsReq > M)
-                    return false;
-            }
-            else 
-                currSum += A[i];
+bool isPossible(int pages, vector<int>& A, int students) {
+    ll sum = 0;
+    for(int i = 0; i < A.size(); i++) {
+        if(A[i] > pages)
+            return false;
+        sum += A[i];
+        if(sum > pages) {
+            students--;
+            sum = A[i];
         }
-        return true;
     }
-     
-    int Solution::books(vector<int> &A, int M) {
-        long sum = 0;
-        if(A.size() < M)
-            return -1;
-        for(int i=0; i < A.size(); i++) {
-            sum += A[i];
-        }
-        int start = 0, end = sum;
-        int res = INT_MAX;
-        while(start <= end) {
-            int mid = (start + end) / 2;
-            if(isPossible(A, M, mid)) {
-                res = min(res, mid);
-                end = mid - 1;
-            }
-            else
-                start = mid + 1;
-        }
-        return res;
-    }
+    if(sum > pages)
+        students--;
+    return students > 0;
+}
 
+int Solution::books(vector<int> &A, int B) {
+    ll l = 1, h = 0;
+    if(B > A.size())
+        return -1;
+    for(int i = 0; i < A.size(); i++)
+        h += A[i];
+    while(l <= h) {
+        int mid = l + (h - l) / 2;
+        if(isPossible(mid, A, B) && !isPossible(mid - 1, A, B))
+            return mid;
+        else if(!isPossible(mid, A, B))
+            l = mid + 1;
+        else
+            h = mid - 1;
+    }
+    return isPossible(h, A, B) ? h : -1;
+}
 
