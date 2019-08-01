3a679fd15fb7fbb8127e852a0e820045e247e77f
Gagandeep Singh Ahuja <gagandeepahuja09@gmail.com>
2019-08-01 18:50:42 +0530

    easy

--- InterviewBit/SortedInsertPosition.cpp
+++ InterviewBit/SortedInsertPosition.cpp
@@ -1,19 +1,13 @@
-int Solution::searchInsert(vector<int> &A, int target) {
-    int low = 0, high = A.size() - 1;
-    if(target < A[low])
-        return low;
-    if(target > A[high])
-        return high + 1;
-    // Return the index of least element >= target
-    // Lower Bound
-    while(low < high) {
-        int mid = (low + high)/2;
-        if(A[mid] < target)
-            low = mid + 1;
-        else if(A[mid] > target)
-            high = mid;
+int Solution::searchInsert(vector<int> &A, int B) {
+    int l = 0, h = A.size();
+    while(l <= h) {
+        int m = l + (h - l) / 2;
+        if(A[m] == B || ((m == 0 || A[m - 1] < B) && (m  == A.size() || A[m] > B)))
+            return m;
+        else if(A[m] < B)
+            l = m + 1;
         else
-            return mid;
+            h = m - 1;
     }
-    return low;
+    return h;
 }
