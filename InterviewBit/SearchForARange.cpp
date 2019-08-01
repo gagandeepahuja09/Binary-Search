239f410d1432ecaf9906d8b0d4d24e4adf2793aa
Gagandeep Singh Ahuja <gagandeepahuja09@gmail.com>
2019-08-01 14:35:45 +0530

    easy IMP

--- InterviewBit/SearchForARange.cpp
+++ InterviewBit/SearchForARange.cpp
@@ -1,36 +1,40 @@
-int upperBound(const vector<int>& A, int B, int low, int high) {
-    if(low > high) {
-        return -1;    
+int lowerBound(const vector<int>& A, int target) {
+    int l = 0, h = A.size() - 1;
+    while(l <= h) {
+        int m = l + (h - l) / 2;
+        if(A[m] == target && (m == 0 || A[m] > A[m - 1])) {
+            return m;
+        }
+        else if(A[m] < target) {
+            l = m + 1;
+        }
+        else {
+            h = m - 1;
+        }
     }
-    int mid = low + (high - low)/2;
-    if(A[mid] == B && (mid == A.size() - 1 || B < A[mid + 1]))
-        return mid;
-    else if(A[mid] > B) {
-        return upperBound(A, B, low, mid - 1);
-    }
-    else {
-        return upperBound(A, B, mid + 1, high);
-    }
+    return -1;
 }
 
-int lowerBound(const vector<int>& A, int B, int low, int high) {
-    if(low > high) {
-        return -1;    
+int upperBound(const vector<int>& A, int target) {
+    int l = 0, h = A.size() - 1;
+    while(l <= h) {
+        int m = l + (h - l) / 2;
+        if(A[m] == target && (m == A.size() - 1 || A[m] < A[m + 1])) {
+            return m;
+        }
+        else if(A[m] > target) {
+            h = m - 1;
+        }
+        else {
+            l = m + 1;
+        }
     }
-    int mid = low + (high - low)/2;
-    if(A[mid] == B && (mid == 0 || B > A[mid - 1]))
-        return mid;
-    else if(A[mid] < B) {
-        return lowerBound(A, B, mid + 1, high);
-    }
-    else {
-        return lowerBound(A, B, low, mid - 1);
-    }
+    return -1;
 }
 
 vector<int> Solution::searchRange(const vector<int> &A, int B) {
-    int u = upperBound(A, B, 0, A.size() - 1);
-    int l = lowerBound(A, B, 0, A.size() - 1);
-    return { l, u };
+    int l = lowerBound(A, B);
+    int r = upperBound(A, B);
+    return { l, r };
 }
 
