443226424918e4d6bb0661f33bc9006dd6afe07a
Gagandeep Singh Ahuja <gagandeepahuja09@gmail.com>
2019-08-01 10:41:38 +0530

    imp

--- InterviewBit/SquareRootOfInteger.cpp
+++ InterviewBit/SquareRootOfInteger.cpp
@@ -1,17 +1,16 @@
 int Solution::sqrt(int A) {
     if(A == 0 || A == 1)
         return A;
-    int l = 1, h = A, ans;
+    int l = 1, h = A;
     while(l <= h) {
         int m = l + (h - l) / 2;
         if(m == A / m)
             return m;
         else if(m < A / m) {
-            ans = m;
             l = m + 1;
         }
         else
             h = m - 1;
     }
-    return ans;
+    return h;
 }
