d0fd2ba17094bf19b970aea7bc8324af215b574c
Gagandeep Singh Ahuja <gagandeepahuja09@gmail.com>
2019-08-01 10:15:20 +0530

    good question

--- InterviewBit/MatrixMedian.cpp
+++ InterviewBit/MatrixMedian.cpp
@@ -1,26 +1,19 @@
-int Solution::findMedian(vector<vector<int>> &A) {
-    int low = INT_MIN, high = INT_MAX;
-    int r = A.size(), c = A[0].size();
-    for(int i=0; i<r; i++) {
-        if(A[i][0] < low)
-            low = A[i][0];
-        if(A[i][c - 1] > high)
-            high = A[i][c - 1];
+int Solution::findMedian(vector<vector<int> > &A) {
+    int l = INT_MAX, h = INT_MIN, m = A.size(), n = A[0].size();
+    for(int i = 0; i < m; i++) {
+        l = min(l, A[i][0]);
+        h = max(h, A[i][n - 1]);
     }
-    int desired = (r * c + 1) / 2;
-    while(low < high) {
-        int mid = low + (high - low) / 2;
-        int place = 0;
-        // Finding count of all elements greater than mid
-        for(int i=0; i < r; i++) {
-            place += upper_bound(A[i].begin(), A[i].end(), mid) -
-            A[i].begin();
+    while(l < h) {
+        int mid = l + (h - l) / 2, cnt = 0;
+        for(int i = 0; i < m; i++) {
+            cnt += upper_bound(A[i].begin(), A[i].end(), mid) - A[i].begin();
         }
-        if(place < desired)
-            low = mid + 1;
+        if(cnt < ((m * n + 1)/ 2))
+            l = mid + 1;
         else
-            high = mid;
+            h = mid;
     }
-    return low;
+    return l;
 }
 
