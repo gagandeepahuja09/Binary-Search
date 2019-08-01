c1e7a21ab3f976e6c7ae6e6f1adb27bcb3c99e41
Gagandeep Singh Ahuja <gagandeepahuja09@gmail.com>
2019-08-01 14:04:24 +0530

    IMP

--- InterviewBit/MatrixSearch.cpp
+++ InterviewBit/MatrixSearch.cpp
@@ -1,38 +1,35 @@
-int bSearch(vector<vector<int>>& A, int r, int l, int h, int target) {
+bool bSearch(vector<vector<int>>& A, int r, int k) {
+    int l = 0, h = A[r].size() - 1;
     while(l <= h) {
-        int mid = l + (h - l) / 2;
-        if(A[r][mid] == target)
+        int m = l + (h - l) / 2;
+        if(A[r][m] == k) 
             return 1;
-        else if(A[r][mid] < target)
-            l = mid + 1;
-        else
-            h = mid - 1;
+        if(A[r][m] < k)
+            l = m + 1;    
+        else 
+            h = m - 1;
     }
     return 0;
 }
 
-int Solution::searchMatrix(vector<vector<int> > &A, int target) {
-    int m = A.size(), n = A[0].size() - 1;
-    int l = 0, h = A.size() - 1;
-    while(h - l > 1) {
-        int mid = l + (h - l) / 2;
-        if(A[mid][n / 2] == target)
-            return 1;
-        else if(A[mid][n / 2] < target)
-            l = mid;
-        else
-            h = mid;
+int Solution::searchMatrix(vector<vector<int> > &A, int B) {
+    int r = A.size(), c = A[0].size();
+    int l = 0, h = r - 1;
+    while(l <= h) {
+        if(l == h) {
+            return bSearch(A, l, B);
+        }
+        int m = l  + (h - l) / 2;
+        if(A[m][0] <= B && A[m][c - 1] >= B) {
+            return bSearch(A, m, B);
+        }
+        else if(A[m][0] > B) {
+            h = m - 1;
+        }
+        else {
+            l = m + 1;
+        }
     }
-    // cout << l << " " << h << endl;
-    if(A[l][n / 2] == target || A[h][n / 2] == target)
-        return 1;
-    if(target >= A[l][0] && target <= A[l][n / 2])
-        return bSearch(A, l, 0, n / 2, target);
-    else if(target >= A[l][n / 2] && target <= A[l][n])
-        return bSearch(A, l, n / 2, n, target);
-    else if(target >= A[h][0] && target <= A[h][n / 2])
-        return bSearch(A, h, 0, n / 2, target);
-    else
-        return bSearch(A, h, n / 2, n, target);
+    return 0;
 }
 
