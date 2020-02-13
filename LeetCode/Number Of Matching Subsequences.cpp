cfe19e9b056cfff6f294a9ea5b03c3d5f1704dd0
Gagandeep Singh Ahuja <36494305+gagandeepahuja09@users.noreply.github.com>
2020-02-13 15:29:43 +0530

    IMP revised

--- LeetCode/Number Of Matching Subsequences.cpp
+++ LeetCode/Number Of Matching Subsequences.cpp
@@ -1,24 +1,25 @@
 class Solution {
 public:
-    int numMatchingSubseq(string S, vector<string>& words) {
-        vector<vector<int>> alpha(26);
-        for(int i = 0; i < S.size(); i++) {
-            alpha[S[i] - 'a'].push_back(i);
+    int numMatchingSubseq(string s, vector<string>& words) {
+        int ret = 0;
+        vector<vector<int>> cnt(26);
+        for(int i = 0; i < s.size(); i++) {
+            cnt[s[i] - 'a'].push_back(i);
         }
-        int ans = 0;
-        for(auto word : words) {
-            bool found = true;
-            int x = -1;
-            for(char c : word) {
-                auto it = upper_bound(alpha[c - 'a'].begin(), alpha[c - 'a'].end(), x);
-                if(it == alpha[c - 'a'].end())
-                    found = false;
-                else
-                    x = *it;
+        
+        for(string curr : words) {
+            int pos = 0, i = 0, flag = true;
+            for(; i < curr.size(); i++) {
+                int idx = curr[i] - 'a';
+                auto it = lower_bound(cnt[idx].begin(), cnt[idx].end(), pos);
+                if(it == cnt[idx].end()) {
+                    flag = false;
+                    break;
+                }
+                pos = (*it) + 1;
             }
-            if(found)
-                ans++;
+            ret += flag;
         }
-        return ans;
+        return ret;
     }
 };
