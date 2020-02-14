463e2e00220b9b9ac838a514a73a3b0fce142a4e
Gagandeep Singh Ahuja <36494305+gagandeepahuja09@users.noreply.github.com>
2020-02-14 13:50:21 +0530

    easy revised

--- LeetCode/Split Array Largest Sum.cpp
+++ LeetCode/Split Array Largest Sum.cpp
@@ -1,30 +1,31 @@
 class Solution {
 public:
-    bool isPossible(vector<int>& nums, long m, int k) {
-        long sum = 0;
-        int cnt = 1;
-        for(int i = 0; i < nums.size(); i++) {
-            sum += nums[i];
-            if(sum > m) {
-                sum = nums[i];
-                cnt++;
+    bool canSplit(vector<int>& A, long target, long req) {
+        long sum = 0, partitions = 0;
+        for(int i = 0; i < A.size(); i++) {
+            sum += (long)A[i];
+            if(sum > target) {
+                partitions++;
+                sum = A[i];
             }
         }
-        return (cnt <= k);
+        return (partitions < req);
     }
     
-    int splitArray(vector<int>& nums, int k) {
-        long h = 0, l = 0;
-        for(int i : nums)
-            h += i, l = max(l, (long)i);
-        while(l <= h) {
-            long m = l + (h - l) / 2;
-            if(isPossible(nums, m, k)) {
-                h = m - 1;
+    int splitArray(vector<int>& A, int m) {
+        long low = (long)*max_element(A.begin(), A.end());
+        long high = (long)accumulate(A.begin(), A.end(), 0), ans = high;
+        while(low <= high) {
+            long mid = low + (high - low) / 2;
+            //       cout << mid << endl;
+            if(canSplit(A, mid, m)) {
+                ans = mid;
+                high = mid - 1;
             }
-            else
-                l = m + 1;
+            else {
+                low = mid + 1;
+            }
         }
-        return (int)l;
+        return (int)ans;
     }
 };
