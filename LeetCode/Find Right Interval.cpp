e8b4fbeaeb9a0e437ce9e672c84073f551f75c34
Gagandeep Singh Ahuja <36494305+gagandeepahuja09@users.noreply.github.com>
2020-02-15 15:59:36 +0530

    IMP revised

--- LeetCode/Find Right Interval.cpp
+++ LeetCode/Find Right Interval.cpp
@@ -1,17 +1,16 @@
 class Solution {
 public:
-    vector<int> findRightInterval(vector<vector<int>>& in) {
-        map<int, int> mp;
-        for(int i = 0; i < in.size(); i++) {
-            mp[in[i][0]] = i;
+    vector<int> findRightInterval(vector<vector<int>>& ins) {
+        map<int, int> hash;
+        int n = ins.size();
+        vector<int> ret(n, -1);
+        for(int i = 0; i < ins.size(); i++) {
+            hash[ins[i][0]] = i;
         }
-        vector<int> ret;
-        for(int i = 0; i < in.size(); i++) {
-            auto it = mp.lower_bound(in[i][1]);
-            if(it == mp.end())
-                ret.push_back(-1);
-            else
-                ret.push_back(it -> second);
+        for(int i = 0; i < ins.size(); i++) {
+            auto minLeft = hash.lower_bound(ins[i][1]);
+            if(minLeft != hash.end())
+                ret[i] = minLeft -> second;
         }
         return ret;
     }
